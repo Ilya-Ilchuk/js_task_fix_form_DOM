3a9e52a0d2b7e8a936bd37a22530c19d6761d8d8
Ilya-Ilchuk <illya.illchuk@gmail.com>
2025-01-01 22:49:35 +0200

    make build

---
+++ index.cbd41a14.js
@@ -0,0 +1,2 @@
+for(var e=["First name","Last name","Email","Password","Email","Password"],t=document.getElementsByClassName("field"),l=document.querySelectorAll("input"),a=0;a<t.length;a++){var r=document.createElement("label");r.className="field-label",r.textContent=e[a],l[a].setAttribute("placeholder",e[a]),t[a].insertBefore(r,t[a].firstChild)}
+//# sourceMappingURL=index.cbd41a14.js.map
