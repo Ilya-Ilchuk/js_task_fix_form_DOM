7bb10437277151698ef7b0a580b247feddb64287
Ilya-Ilchuk <illya.illchuk@gmail.com>
2025-01-01 22:49:12 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const labelNames = [
+  'First name',
+  'Last name',
+  'Email',
+  'Password',
+  'Email',
+  'Password',
+];
+
+const field = document.getElementsByClassName('field');
+const input = document.querySelectorAll('input');
+
+for (let i = 0; i < field.length; i++) {
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.textContent = labelNames[i];
+  input[i].setAttribute('placeholder', labelNames[i]);
+
+  field[i].insertBefore(label, field[i].firstChild);
+}
